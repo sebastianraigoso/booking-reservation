23becb0c973d0bcc47819ba72d4ddc83cb071cce
sebastian <sebastianraigoso.dev@gmail.com>
2026-03-24 14:48:45 +0900

    connect backend

---
+++ server/server.js
@@ -0,0 +1,30 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+let bookings = []
+
+app.get('/', (req, res) => {
+  res.send('API running')
+})
+
+app.post('/bookings', (req, res) => {
+  const booking = req.body
+  bookings.push(booking)
+
+  res.json({
+    message: 'Booking created',
+    booking
+  })
+})
+
+app.get('/bookings', (req, res) => {
+  res.json(bookings)
+})
+
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:3000')
+})
